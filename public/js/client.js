6c18e8955a711095d09eecafe5da93d0b48d2fd0
Gerpiili369 <elektromiikka@hotmail.com>
2017-11-16 10:20:59 +0200

    Pictures are now determinded by updateVisuals() instead of socket.on()

--- public/js/client.js
+++ public/js/client.js
@@ -1,8 +1,12 @@
+let theme = "defeault";
+let mem = {player: {selection: null, result: null}, opponent: null}
+
 document.addEventListener("DOMContentLoaded", initialize)
 
 function initialize() {
   let socket = io();
-  let theme = "horror";
+
+  updateVisuals(theme);
 
   document.getElementById("rockbtn").addEventListener("click", function() {
     socket.emit('choose', "rock");
@@ -15,13 +19,17 @@ function initialize() {
   });
   document.getElementById('classicactivate').addEventListener("click", function() {
     theme = "defeault";
+    updateVisuals(theme);
   });
   document.getElementById('horroractivate').addEventListener("click", function() {
     theme = "horror"
+    updateVisuals(theme);
   })
 
   socket.on('result', (player, opponent, games) => {
-    console.log("points: "+player.points+", your: "+player.selection+", opponents: "+opponent+", games"+games)
+    mem.player = player
+    mem.opponent = opponent
+
     document.getElementById("winBad").innerHTML = player.points.wins;
     document.getElementById("drawBad").innerHTML = player.points.draws;
     document.getElementById("lossBad").innerHTML =  player.points.losses;
@@ -29,13 +37,19 @@ function initialize() {
     document.getElementById("winBar").style = "width: "+(player.points.wins/games*100)+"%";
     document.getElementById("drawBar").style = "width: "+(player.points.draws/games*100)+"%";
     document.getElementById("lossBar").style = "width: "+(player.points.losses/games*100)+"%";
-    document.getElementById("playerpicture").src = "img/"+theme+"/"+player.selection+".png";
-    document.getElementById("opponentpicture").src = "img/"+theme+"/"+opponent+".png";
-    document.getElementById("result").src = "img/"+theme+"/"+player.result+".png";
-    document.getElementById("vs").src = "img/"+theme+"/vs.png";
-    //document.getElementById("background").style.backgroundImage = "img/"+theme+"/background.png";
 
-    document.getElementById("hideatstart1").hidden = false;
-    document.getElementById("hideatstart2").hidden = false;
+    updateVisuals(theme);
   });
+}
+
+function updateVisuals(theme) {
+  document.getElementById("playerpicture").src = "img/"+theme+"/"+mem.player.selection+".png";
+  document.getElementById("opponentpicture").src = "img/"+theme+"/"+mem.opponent+".png";
+  document.getElementById("result").src = "img/"+theme+"/"+mem.player.result+".png";
+  document.getElementById("vs").src = "img/"+theme+"/vs.png";
+  document.getElementById("stylesheet").href = "css/"+theme+".css"
+
+  if (mem.player.result != null) {
+    document.getElementById("hideatstart").hidden = false;
+  }
 }
