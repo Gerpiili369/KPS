1ae7e1738a749145ff08d816ac8718e8407c1a35
Gerpiili369 <elektromiikka@hotmail.com>
2017-11-21 14:27:03 +0200

    re-made server-side logs

--- index.js
+++ index.js
@@ -37,29 +37,45 @@ let players = {
   }}
 
 io.on('connection', socket => {
-  if (players.p1.id == null) players.p1.id = socket.id;
-  else if (players.p2.id == null) players.p2.id = socket.id;
-  else socket.emit('msgFromServer', 'Game full!');
-
+  if (players.p1.id == null) {
+    players.p1.id = socket.id;
+    console.log(socket.id+" connected as Player 1");
+  }
+  else if (players.p2.id == null) {
+    players.p2.id = socket.id;
+    console.log(socket.id+" connected as Player 2");
+  }
+  else {
+    socket.emit('msgFromServer', 'Game full!');
+    console.log(socket.id+" connected but game is full");
+  }
   socket.on('choose', data => {
-    if (players.p1.id == socket.id) players.p1.selection = data;
-    else if (players.p2.id == socket.id) players.p2.selection = data;
-    console.log(players)
+    if (players.p1.id == socket.id) {
+      players.p1.selection = data;
+      console.log("Player 1 chose "+data);
+    }
+    else if (players.p2.id == socket.id) {
+      players.p2.selection = data;
+      console.log("Player 2 chose "+data);
+    }
     if (players.p1.selection != null && players.p2.selection != null) {
       games ++
       let result = game.decider(players.p1.selection, players.p2.selection)
-      console.log(result)
+
       if (result == 'p1') {
         players.p1.points.wins ++; players.p1.result = "win";
         players.p2.points.losses ++; players.p2.result = "defeat";
+        console.log("Player 1 has won the round");
       }
       if (result == 'p2') {
         players.p1.points.losses ++; players.p1.result = "defeat";
         players.p2.points.wins ++; players.p2.result = "win";
+        console.log("Player 2 has won the round");
       }
       if (result == 'draw') {
         players.p1.points.draws ++; players.p1.result = "draw";
         players.p2.points.draws ++; players.p2.result = "draw";
+        console.log("The round was a draw");
       }
 
       io.to(players.p1.id).emit('result', players.p1, players.p2.selection, games);
@@ -67,6 +83,7 @@ io.on('connection', socket => {
 
       players.p1.selection = null; players.p1.result = null;
       players.p2.selection = null; players.p2.result = null;
+      console.log("A new round has been started!");
     }
   });
 
@@ -75,8 +92,14 @@ io.on('connection', socket => {
     players.p1.points = {wins: 0, losses: 0, draws: 0}
     players.p2.points = {wins: 0, losses: 0, draws: 0}
 
-    if (players.p1.id == socket.id) players.p1.id = null;
-    else if (players.p2.id == socket.id) players.p2.id = null;
+    if (players.p1.id == socket.id) {
+      players.p1.id = null;
+      console.log("Player 1 left");
+    }
+    else if (players.p2.id == socket.id) {
+      players.p2.id = null;
+      console.log("Player 2 left");
+    }
   });
 });
 
